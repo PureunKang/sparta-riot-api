ab0f2758cc92490509c73658c8242188137cd308
PureunKang <pureun0000@gmail.com>
2025-03-10 15:49:01 +0900

    chore : 파일 구조 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
@@ -6,5 +6,5 @@ export default function Home() {
       <Button>Click me</Button>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  );
+  )
 }
