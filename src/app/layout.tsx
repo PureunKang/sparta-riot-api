f3ffb737d1aa62618e72478d7b21b45d0fdf8d6b
PureunKang <pureun0000@gmail.com>
2025-03-10 16:14:57 +0900

    feat : 라우팅 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import Link from "next/link"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav className="flex justify-evenly">
+          <Link href="/">홈</Link>
+          <Link href="/champions">챔피언 목록</Link>
+          <Link href="/items">아이템 목록</Link>
+          <Link href="/rotation">챔피언 로테이션</Link>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
