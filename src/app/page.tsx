3bc32ab71ea5d1940cc7d3848bf0fd9b49ae124c
PureunKang <pureun0000@gmail.com>
2025-03-10 15:32:06 +0900

    project : 세팅 및 적용 테스트

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
