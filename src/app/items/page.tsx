5f7f2adaa5b372300028e3b20d7bc7a53ddc2e5f
PureunKang <pureun0000@gmail.com>
2025-03-11 17:53:52 +0900

    feat : 아이템 목록 불러오기

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,11 +1,12 @@
+import ItemCard from "@/components/ItemCard"
 import React from "react"
-import { Item } from "@/types/Item"
-import { fetchItemList } from "@/utils/serverApi"
 
 const page = () => {
-  // fetchItemList().then((data) => console.log("아이템", data))
-
-  return <div>아이템 목록</div>
+  return (
+    <>
+      <ItemCard />
+    </>
+  )
 }
 
 export default page
---
+++ src/components/ItemCard.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { fetchItemList } from "@/utils/serverApi"
+import Image from "next/image"
+
+const ItemCard = async () => {
+  const rawData = await fetchItemList()
+  const itemList = Object.values(rawData.data)
+
+  return (
+    <>
+      {itemList.map((i) => {
+        return (
+          <Card key={i.id}>
+            <CardHeader>
+              <CardTitle>{i.name}</CardTitle>
+              <CardDescription>가격 | {i.gold.total}</CardDescription>
+              <CardDescription>팔때 | {i.gold.sell}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>이미지 넣을 곳</p>
+            </CardContent>
+          </Card>
+        )
+      })}
+    </>
+  )
+}
+
+export default ItemCard
