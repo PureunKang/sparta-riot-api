4200059348d1636166c14456d15d9d9e8726e46c
PureunKang <pureun0000@gmail.com>
2025-03-11 16:24:46 +0900

    feat : 챔피언 목록 데이터 확인

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,6 +1,10 @@
 import React from "react"
+import { fetchChampionList } from "@/utils/serverApi"
+import { Champion } from "@/types/Champion"
 
 const page = () => {
+  fetchChampionList().then((data) => console.log("데이터확인", data))
+
   return <div>챔피언 목록</div>
 }
 
---
+++ src/utils/serverApi.ts
@@ -0,0 +1,15 @@
+// 추후 Server Action으로 변경 위한 선언
+"use server"
+
+export const fetchChampionList = async () => {
+  const res = await fetch(
+    "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json",
+    {
+      next: {
+        revalidate: 86400,
+      },
+    },
+  )
+  const data = await res.json()
+  return data
+}
