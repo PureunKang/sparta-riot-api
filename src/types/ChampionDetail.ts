7143fb193a0044a534dffe232e31381648f0ea6a
PureunKang <pureun0000@gmail.com>
2025-03-12 17:50:59 +0900

    feat : 챔피언 디테일 타입 정의

--- src/types/ChampionDetail.ts
+++ src/types/ChampionDetail.ts
@@ -1,45 +1,22 @@
 export type ChampionDetail = {
-  data: Array<string | number>
-  version: string
   id: string
   key: string
   name: string
   title: string
-  blurb: string
-  info: {
-    attack: number
-    defense: number
-    magic: number
-    difficulty: number
-  }
   image: {
     full: string
     sprite: string
-    group: string
-    x: number
-    y: number
-    w: number
-    h: number
   }
-  tags: Array<string>
-  partype: string
-  stats: {
-    hp: number
-    hpperlevel: number
-    mp: number
-    mpperlevel: number
-    movespeed: number
-    armor: number
-    armorperlevel: number
-    spellblock: number
-    spellblockperlevel: number
-    mpregen: number
-    mpregenperlevel: number
-    crit: number
-    critperlevel: number
-    attackdamage: number
-    attackdamageperlevel: number
-    attackspeedperlevel: number
-    attackspeed: number
-  }
+  lore: string
+  spells: [
+    {
+      id: string
+      name: string
+      description: string
+    },
+    image: {
+      full: string
+      sprite: string
+    },
+  ]
 }
