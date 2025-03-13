856bfb8d926395fbfcde35b1f4f4a43cad6c3d05
PureunKang <pureun0000@gmail.com>
2025-03-14 01:32:05 +0900

    feat : 라우트 핸들러 1차

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,13 +1,38 @@
+import { Champion } from "@/types/Champion"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
   const apiKey = process.env.RIOT_API_KEY
 
+  // 이달의 챔피언 키값 불러오기
   const rotationsRes = await fetch(
     `https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${apiKey}`,
     {},
   )
-  const rotationData = await rotationRes.json()
+  const rotationData = await rotationsRes.json()
+  const championIds: Array<number> = rotationData.freeChampionIds
 
-  return NextResponse.json(data)
+  // 챔피언 데이터 불러오기
+  const ddragonRes = await fetch(
+    "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json",
+  )
+  const ddragonData = await ddragonRes.json()
+  const allChampions = Object.values(ddragonData.data)
+
+  // 불러온 챔피언 데이터에서 일치하는 키값 골라내기
+  const championInfo = championIds
+    .map((champId) => {
+      const selectedChampion = allChampions.find((champ: Champion) => champ.key === String(champId))
+      if (selectedChampion) {
+        return {
+          name: selectedChampion.name,
+          title: selectedChampion.title,
+          image: selectedChampion.image.full,
+        }
+      }
+      return null
+    })
+    .filter((champ) => champ !== null)
+
+  return NextResponse.json(championInfo)
 }
