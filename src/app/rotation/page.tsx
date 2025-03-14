39092894d7c8d8b3db1570efaa6dfe135f5ef5af
PureunKang <pureun0000@gmail.com>
2025-03-14 14:50:03 +0900

    로테이션 페이지 렌더링

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,8 +1,45 @@
 "use client"
+
+import { useEffect, useState } from "react"
 import React from "react"
 
+// {
+//   "name": "알리스타",
+//   "title": "미노타우로스",
+//   "image": "Alistar.png"
+// },
+
+type SelectedChampion = {
+  name: string
+  title: string
+  image: string
+}
+
 const page = () => {
-  return <>로테이션페이지</>
+  const [champions, setChampions] = useState<SelectedChampion[]>([])
+
+  useEffect(() => {
+    async function fetchSelectedChampions() {
+      const res = await fetch("/api/rotation")
+      const data = await res.json()
+      setChampions(data)
+    }
+    fetchSelectedChampions()
+  }, [])
+
+  return (
+    <>
+      {champions.map((c) => {
+        return (
+          <>
+            <div>{c.name}</div>
+            <div>{c.title}</div>
+            <p>이미지 넣을 곳</p>
+          </>
+        )
+      })}
+    </>
+  )
 }
 
 export default page
