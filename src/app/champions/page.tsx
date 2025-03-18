6a833c7f9dc9a7dc8a52fd2f9cc14738447157ab
PureunKang <pureun0000@gmail.com>
2025-03-18 15:34:08 +0900

    style : 챔피언 페이지 레이아웃

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -7,7 +7,10 @@ const ChampionPage = async () => {
 
   return (
     <>
-      <ChampionCard championList={championList} />
+      <div className="container mx-auto p-6">
+        <h1 className="text-2xl font-bold mb-4">챔피언 목록</h1>
+        <ChampionCard championList={championList} />
+      </div>
     </>
   )
 }
